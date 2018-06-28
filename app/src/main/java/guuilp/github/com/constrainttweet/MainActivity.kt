3cfe9de151fbf167f3d2574860f410e14db809a5
Guilherme Lima Pereira <guuuilp@gmail.com>
2018-06-27 23:26:37 -0300

    Initial commit

---
+++ app/src/main/java/guuilp/github/com/constrainttweet/MainActivity.kt
@@ -0,0 +1,12 @@
+package guuilp.github.com.constrainttweet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
